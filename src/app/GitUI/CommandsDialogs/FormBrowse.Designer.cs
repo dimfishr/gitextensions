d7e3265757f66b5fc82e8a89f1c1ba6a06c033da
dimfish <dimfish@gmail.com>
2021-12-10 18:38:10 +0300

    Fix Test

--- src/app/GitUI/CommandsDialogs/FormBrowse.Designer.cs
+++ src/app/GitUI/CommandsDialogs/FormBrowse.Designer.cs
@@ -1446,7 +1446,7 @@ namespace GitUI.CommandsDialogs
 
         #endregion
 
-        private SplitContainer MainSplitContainer;
+        internal SplitContainer MainSplitContainer;
         private SplitContainer RightSplitContainer;
         private SplitContainer RevisionsSplitContainer;
 
